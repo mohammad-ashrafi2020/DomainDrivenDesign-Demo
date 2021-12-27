bb8d29166454da2817e87cd3e9515452871c83d0
Mohammad Ashrafi <Mohammad.ashrafi2020@outlook.com>
2021-12-27 16:54:30 +0330

    Section 28 - Fluent Assertion

--- Calculator.Tests.Unit/ComputingTests.cs
+++ Calculator.Tests.Unit/ComputingTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,25 +10,47 @@ namespace Calculator.Tests.Unit
 {
     public class ComputingTests
     {
+        Computing computing;
+
+        public ComputingTests()
+        {
+            computing = new();
+        }
         [Fact]
         public void OddOrEvent_Should_Return_Odd_When_Input_is_OddValue()
         {
-            var computing = new Computing();
-
             var result = computing.OddOrEven(11);
 
-            Assert.Equal("Odd", result);
+            result.Should().Be("Odd");
         }
 
         [Theory]
         [InlineData(10)]
         public void OddOrEvent_Should_Return_Even_When_Input_is_EvenValue(int value)
         {
-            var computing = new Computing();
-
             var result = computing.OddOrEven(value);
-
-            Assert.Equal("Even", result);
+            result.Should().Be("Even");
+        }
+        [Fact]
+        public void CalculateAge_Should_Calculate_Age_When_CurrentYear_And_BirthDate_Is_Valid_Data()
+        {
+            var result = computing.CalculateAge(1380, 1400);
+            result.Should().Be(20);
+        }
+        [Fact]
+        public void CalculateAge_Should_Return_Zero_When_Birth_LessThan_Zero()
+        {
+            var result = computing.CalculateAge(-1, 1400);
+            result.Should().Be(0);
+        }
+        [Fact]
+        public void CalculateAge_Should_Throw_ArgumentException_When_Birth_Or_CurrentYear_Is_Zero()
+        {
+            var result = new Action(() =>
+              {
+                  computing.CalculateAge(0, 1400);
+              });
+            result.Should().Throw<ArgumentException>();
         }
     }
 }
--- Calculator/Computing.cs
+++ Calculator/Computing.cs
@@ -6,5 +6,31 @@
         {
             return value % 2 == 0 ? "Even" : "Odd";
         }
+        public int CalculateAge(int birthDate, int currentYear)
+        {
+            if (birthDate < 0)
+                return 0;
+
+            if (birthDate == 0 || currentYear == 0)
+                throw new ArgumentException();
+
+            return currentYear - birthDate;
+        }
+        public bool IsPrime(int value)
+        {
+            if (value < 2)
+            {
+                return false;
+            }
+
+            for (int divisor = 2; divisor <= Math.Sqrt(value); ++divisor)
+            {
+                if (value % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
